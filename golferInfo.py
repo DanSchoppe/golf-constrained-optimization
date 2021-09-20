15d865560f80639907603d3b30e751a87cc6d001
Dan Schoppe <dan.schoppe@gmail.com>
2021-09-19 23:23:32 -0500

    Add fake golferInfo.py

---
+++ golferInfo.py
@@ -0,0 +1,14 @@
+# Fake data to protect privacy...
+golferInfo = [
+    { 'name': 'Kent', 'vaxed': True, 'concerned': False, 'friends': ['Erik', 'Joe', 'Jeff', 'Reid', 'Brian'] },
+    { 'name': 'Brandon', 'vaxed': False, 'concerned': True, 'friends': ['Jay', 'Dan', 'Brady', 'James'] },
+    { 'name': 'Erik', 'vaxed': True, 'concerned': False, 'friends': ['Joe', 'Jeff', 'Reid', 'Brian'] },
+    { 'name': 'Jay', 'vaxed': False, 'concerned': True, 'friends': [] },
+    { 'name': 'Joe', 'vaxed': True, 'concerned': False, 'friends': [] },
+    { 'name': 'Dan', 'vaxed': False, 'concerned': True, 'friends': [] },
+    { 'name': 'Jeff', 'vaxed': True, 'concerned': False, 'friends': [] },
+    { 'name': 'Brady', 'vaxed': False, 'concerned': True, 'friends': [] },
+    { 'name': 'Reid', 'vaxed': True, 'concerned': False, 'friends': [] },
+    { 'name': 'James', 'vaxed': False, 'concerned': True, 'friends': [] },
+    { 'name': 'Brian', 'vaxed': True, 'concerned': False, 'friends': [] },
+]
