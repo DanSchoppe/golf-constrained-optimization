1c585b166df0ba4bccff3e5cc9d13911fdf98019
Dan Schoppe <dan.schoppe@gmail.com>
2022-09-27 00:56:30 -0500

    Remove covid constraints, add riding alone constraint, improve logging

--- groups.py
+++ groups.py
@@ -6,22 +6,22 @@ import numpy as np
 from golferInfo import golferInfo
 
 def main():
-  rounds = range(5)
+  games = ['Wolf', 'Small Scramble', 'Yellow Ball', 'Large Scramble', 'Individual']
+  rounds = range(len(games))
   teams = range(3)
   carts = range(2)
+
+  # Carts are double occupancy except:
+  singleOccupancyCarts = [
+    { 'team': 0, 'cart': 1 },
+    { 'team': 1, 'cart': 1 }
+  ]
 
   golfers = [golfer['name'] for golfer in golferInfo]
   golferCombinations = list(combinations(golfers, 2))
 
-  restrictedCombinations = {}
   friendCombinations = {}
   for g1, g2 in combinations(golferInfo, 2):
-    restricted = (
-      (not g1['vaxed'] and g2['concerned']) or
-      (g1['concerned'] and not g2['vaxed'])
-    )
-    restrictedCombinations[(g1['name'], g2['name'])] = restricted
-
     friends = (
       g1['name'] in g2['friends'] or
       g2['name'] in g1['friends']
@@ -42,10 +42,10 @@ def main():
   # Variables to indicate whether golfers are riding together
   # (golfer1, golfer2, round, team, cart) => [0...1]
   ridingTogether = {}
-  for round in rounds:
-    for team in teams:
-      for cart in carts:
-        for golfer1, golfer2 in golferCombinations:
+  for golfer1, golfer2 in golferCombinations:
+    for round in rounds:
+      for team in teams:
+        for cart in carts:
           ridingTogether[(golfer1, golfer2, round, team, cart)] = model.NewBoolVar(f'g{golfer1}+g{golfer2}r{round}t{team}c{cart}')
 
           # Link "ridingTogether" to "cartAssignments":
@@ -56,6 +56,19 @@ def main():
           model.AddBoolOr([x.Not(), y.Not(), p])
           model.AddImplication(p, x)
           model.AddImplication(p, y)
+
+  # Variables to indicate whether golfer is riding alone
+  # (golfer, round) => [0...1]
+  ridingAlone = {}
+  for golfer in golfers:
+    for round in rounds:
+      a = model.NewBoolVar(f'alone+g{golfer}r{round}')
+      ridingAlone[(golfer, round)] = a
+      # Link "ridingAlone" to "cartAssignments"
+      for singleOccupancyCart in singleOccupancyCarts:
+        team = singleOccupancyCart['team']
+        cart = singleOccupancyCart['cart']
+        model.AddImplication(cartAssignments[(golfer, round, team, cart)], a)
 
   # Variables to indicate which team golfers are on for a given round
   # (golfer, round, team) => [0...1]
@@ -76,7 +89,7 @@ def main():
       for golfer1, golfer2 in golferCombinations:
         teamedTogether[(golfer1, golfer2, round, team)] = model.NewBoolVar(f'g{golfer1}+g{golfer2}r{round}t{team}')
 
-        # Link "teamedTogether" to "cartAssignments":
+        # Link "teamedTogether" to "teamAssignments":
         # https://github.com/google/or-tools/blob/master/ortools/sat/doc/boolean_logic.md#product-of-two-boolean-variables
         x = teamAssignments[(golfer1, round, team)]
         y = teamAssignments[(golfer2, round, team)]
@@ -85,44 +98,35 @@ def main():
         model.AddImplication(p, x)
         model.AddImplication(p, y)
 
-  # All but last cart contains two golfers
-  for round in rounds:
-    for team in teams[0:-1]:
-      for cart in carts:
-        model.Add(
-          sum(
-            cartAssignments[(golfer, round, team, cart)] for golfer in golfers
-          ) == 2
-        )
-  for round in rounds:
-    for team in teams[-1:]:
-      for cart in carts[0:-1]:
-        model.Add(
-          sum(
-            cartAssignments[(golfer, round, team, cart)] for golfer in golfers
-          ) == 2
-        )
-      for cart in carts[-1:]:
-        model.Add(
-          sum(
-            cartAssignments[(golfer, round, team, cart)] for golfer in golfers
-          ) == 1
-        )
+  # Define team sizes:
+  def setTeamSize(team, size):
+    for round in rounds:
+      model.Add(
+        sum(
+          teamAssignments[(golfer, round, team)] for golfer in golfers
+        ) == size
+      )
 
-  # All but last team contains four golfers
-  for round in rounds:
-    for team in teams[0:-1]:
+  setTeamSize(team=0, size=3)
+  setTeamSize(team=1, size=3)
+  setTeamSize(team=2, size=4)
+
+  # Define how many riders are in each cart:
+  def setCartOccupancy(team, cart, occupancy):
+    for round in rounds:
       model.Add(
         sum(
-          teamAssignments[(golfer, round, team)] for golfer in golfers
-        ) == 4
+          cartAssignments[(golfer, round, team, cart)] for golfer in golfers
+        ) == occupancy
       )
-    for team in teams[-1:]:
-      model.Add(
-        sum(
-          teamAssignments[(golfer, round, team)] for golfer in golfers
-        ) == 3
+
+  for team in teams:
+    for cart in carts:
+      occupancy = (
+        1 if { 'team': team, 'cart': cart } in singleOccupancyCarts
+        else 2
       )
+      setCartOccupancy(team, cart, occupancy)
 
   # Each golfer is in exactly one cart per round
   for golfer in golfers:
@@ -138,9 +142,11 @@ def main():
     if (
         ('Kent' in [golfer1, golfer2] and 'Reid' in [golfer1, golfer2]) or
         ('Dan' in [golfer1, golfer2] and 'Reid' in [golfer1, golfer2]) or
-        ('Erik' in [golfer1, golfer2] and 'Jeff' in [golfer1, golfer2]) or
+        ('Bob' in [golfer1, golfer2] and 'Jon H' in [golfer1, golfer2]) or
+        ('Bob' in [golfer1, golfer2] and 'Dan' in [golfer1, golfer2]) or
+        ('Jon H' in [golfer1, golfer2] and 'Dan' in [golfer1, golfer2]) or
         ('Erik' in [golfer1, golfer2] and 'Jay' in [golfer1, golfer2]) or
-        ('Jeff' in [golfer1, golfer2] and 'Jay' in [golfer1, golfer2])
+        ('Jon D' in [golfer1, golfer2] and 'James' in [golfer1, golfer2])
     ):
       model.Add(
         sum(ridingTogether[(golfer1, golfer2, round, team, cart)]
@@ -158,25 +164,22 @@ def main():
         ) <= 1
       )
 
-  # Never team with someone four times
+  # Ride alone <=1:
+  for golfer in golfers:
+    model.Add(
+      sum(ridingAlone[(golfer, round)]
+          for round in rounds
+      ) <= 1
+    )
+
+  # Don't team with someone too much:
   for golfer1, golfer2 in golferCombinations:
     model.Add(
       sum(teamedTogether[(golfer1, golfer2, round, team)]
         for round in rounds
         for team in teams
-      ) <= 3
+      ) <= 2
     )
-
-  # COVID preference constraint
-  for golfer1, golfer2 in golferCombinations:
-    if restrictedCombinations[(golfer1, golfer2)]:
-      model.Add(
-        sum(ridingTogether[(golfer1, golfer2, round, team, cart)]
-          for round in rounds
-          for team in teams
-          for cart in carts
-        ) == 0
-      )
 
   # Friendship preference
   model.Maximize(
@@ -202,10 +205,13 @@ def main():
   # solver.parameters.max_time_in_seconds = 100
   solver.Solve(model, cp_model.ObjectiveSolutionPrinter())
 
+  print()
+  print()
   # Results:
   results = np.zeros([len(rounds), len(teams), len(carts), 2], dtype="S10")
   for round in rounds:
-    print('Round', round)
+    print()
+    print(f'Round {round} ({games[round]}):')
     for team in teams:
       print('  Team', team)
       for cart in carts:
@@ -217,25 +223,37 @@ def main():
         for i, rider in enumerate(riders):
           results[round, team, cart, i] = rider
 
+  print()
   # Times teaming and riding together:
-  for golfer1, golfer2 in golferCombinations:
-    print(
-      f'{golfer1} + {golfer2}\t',
-      'riding:',
-      sum(
-        solver.Value(ridingTogether[(golfer1, golfer2, round, team, cart)])
-        for round in rounds
-        for team in teams
-        for cart in carts
-      ),
-      ', teamed:',
-      sum(
-        solver.Value(teamedTogether[(golfer1, golfer2, round, team)])
-        for round in rounds
-        for team in teams
-      )
-    )
+  for golfer1 in golfers:
+    print()
+    print(f'{golfer1}:')
 
+    for golfer2 in [g for g in golfers if g != golfer1]:
+      # We'll print repeated elements (`Dan + Kent` as well as `Kent +
+      # Dan`), but model variables aren't repeated. So find whether we
+      # need to index in the opposite order:
+      invertOrder = (golfer1, golfer2) not in golferCombinations
+      key = (
+        (golfer1, golfer2) if not invertOrder
+        else (golfer2, golfer1)
+      )
+      numRiding = sum(
+        solver.Value(ridingTogether[(*key, round, team, cart)])
+          for round in rounds
+          for team in teams
+          for cart in carts
+      )
+      numTeamed = sum(
+        solver.Value(teamedTogether[(*key, round, team)])
+          for round in rounds
+          for team in teams
+      )
+      print(f'\t{golfer2}\triding: {numRiding}, teamed: {numTeamed}')
+
+  print()
+  print()
+  # CSV output for scorecards:
   np.savetxt(
     sys.stdout.buffer,
     results.astype(np.str_).flatten().reshape([5,12]).transpose(),
